8adbb6abfbca41cddbb357e8b5b21efa281f6ecf
yWarsame <yasinhassan1013@gmail.com>
2026-04-15 21:19:19 +0200

    Customer table query hinzugefügt

---
+++ Retrieve all customer data from the customer table.sql
@@ -0,0 +1,12 @@
+-- one line comment
+
+
+
+/* multi line comment
+   multi line comment
+   multi line comment
+*/
+--Retrieve all customer data from the customers table
+
+Select * 
+From customers
